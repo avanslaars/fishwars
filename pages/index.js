93f8e62a1de1544df228f22d07000e950a519206
Andrew L. Van Slaars <avanslaars@gmail.com>
2017-01-25 19:28:40 -0500

    Clean slate - no more redux

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,12 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { startClock } from '../store'
-import Page from '../components/Page'
-import initRedux from '../redux-decorator'
+import React, {Component} from 'react'
 
-
-@initRedux
-export default class Counter extends React.Component {
-  componentDidMount () {
-    this.timer = this.store.dispatch(startClock())
-  }
-
-  componentWillUnmount () {
-    clearInterval(this.timer)
-  }
-
+export default class App extends Component {
   render () {
     return (
-      <Provider store={this.store}>
-        <Page title='Index Page' linkTo='/other' />
-      </Provider>
+      <div>
+        <h1>Fish Wars</h1>
+        <em>or, Starfish if you prefer</em>
+      </div>
     )
   }
 }
