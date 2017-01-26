d801892d4aace6a06e8a31abf09f01621d43f368
Andrew L. Van Slaars <avanslaars@gmail.com>
2017-01-25 20:22:32 -0500

    Loading people. Getting confused

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,44 @@
 import React, {Component} from 'react'
+import { curry } from 'ramda'
+import 'isomorphic-fetch'
+
+const loadPeople = (people) => (state, props) => ({people: people, isLoading: false})
 
 export default class App extends Component {
+  state = {
+    isLoading: true,
+    people: []
+  }
+
+  componentDidMount() {
+    // Works
+    // fetch('https://swapi-json-server-ddpsgpqivc.now.sh/people')
+    //   .then(res => res.json())
+    //   .then(p => loadPeople(p))
+    //   .then(fn => this.setState(fn))
+
+    //Works
+    fetch('https://swapi-json-server-ddpsgpqivc.now.sh/people')
+      .then(res => res.json())
+      .then(loadPeople)
+      .then(fn => this.setState(fn))
+
+    //Doesn't work - using this.setState without wrapping it in a fn
+    // fetch('https://swapi-json-server-ddpsgpqivc.now.sh/people')
+    //   .then(res => res.json())
+    //   .then(loadPeople)
+    //   .then(this.setState)
+  }
+
   render () {
     return (
       <div>
+        { this.state.isLoading && <h4>Loading Data</h4>}
         <h1>Fish Wars</h1>
         <em>or, Starfish if you prefer</em>
+        <ul>
+          {this.state.people.map(p => <li>{p.name}</li>)}
+        </ul>
       </div>
     )
   }
